c1e30dbf42e6cb05bb65be24304f2138ce654d42
Teng Wu <wutengaaa@163.com>
2022-10-03 00:39:38 +0800

    Autocompletion for brackets

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -16,6 +16,13 @@ keymap('n', '<leader>R', ':Ranger<CR>')
 keymap('n', '<leader>b', ':Buffers<CR>')
 keymap('n', '<leader>m', ':Marks<CR>')
 keymap('n', '<leader>t', ':Tlist<CR>')
+
 vim.cmd([[
 inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
+inoremap ( ()<LEFT>
+inoremap [ []<LEFT>
+inoremap { {}<LEFT>
+inoremap " ""<LEFT>
+inoremap ' ''<LEFT>
+inoremap < <><LEFT>
 ]])
